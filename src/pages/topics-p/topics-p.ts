fcf3c292642cde664899fab4006c7872bb834b02
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2018-04-21 20:22:18 -0500

    Clear out unused checking functionality from professor topics page.

--- src/pages/topics-p/topics-p.ts
+++ src/pages/topics-p/topics-p.ts
@@ -13,25 +13,24 @@ import { FirebaseApp } from 'angularfire2';
 })
 export class TopicsPPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider, public alertCtrl: AlertController, private fbApp: FirebaseApp) {
-
-  	this.lectureName = navParams.get('currLec');
-  	this.className = navParams.get('currClass');
-    console.log('CURRENT LECTURE: ' + this.lectureName)
-    console.log('CURRENT CLASS: ' + this.className)
-    this.topicsRef =  this.fbApp.database().ref('/classes/' + this.className + '/lectures/' + this.lectureName + '/topics/');
-    this.getTopics(); // load up the lecture list
-    this.topicsCheckedMap = new Map([]);
-  }
-
   topicList: Array<any> = [];
   newTopic = '';
   lectureName = '';
   className = '';
   topicsRef: any; // Reference that is frequenly used
   topicsReady: boolean = false; // Check if topics are retrieved before loading list of checkboxes
-  topicsCheckedMap: Map<string, boolean>;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider, public alertCtrl: AlertController, private fbApp: FirebaseApp) {
+
+  	this.lectureName = navParams.get('currLec');
+  	this.className = navParams.get('currClass');
+
+    console.log('CURRENT LECTURE: ' + this.lectureName)
+    console.log('CURRENT CLASS: ' + this.className)
+
+    this.topicsRef =  this.fbApp.database().ref('/classes/' + this.className + '/lectures/' + this.lectureName + '/topics/');
+    this.getTopics(); // load up the lecture list
+  }
 
   ionViewDidLoad() {
   		// not working for some weird reason
@@ -51,13 +50,12 @@ export class TopicsPPage {
 
   getTopics() {
     this.topicsReady = false;
-    this.topicList = []; // this doesn't work - wipe to prevent duplicates from appearing
+    this.topicList = [];
     this.topicsRef.on('value', (snapshot) => {
       snapshot.forEach((child) => {
         this.topicList.push(child.val());
       });
     });
-    console.log(this.topicList)
     console.log("[Alert] Retrieved topics from Firebase.");
     this.topicsReady = true; // Now ready to display...
   }
@@ -97,21 +95,6 @@ export class TopicsPPage {
     });
   }
 
-  updateVote(topicName) {
-    this.setStatus(topicName);
-    var voteChange = 0;
-    if (this.topicsCheckedMap.get(topicName)) { voteChange = 1; }
-    else { voteChange = -1; }
-
-    var topicRef = this.topicsRef.child(topicName);
-    topicRef.transaction(function(currentTopic) {
-      currentTopic.voteCount += voteChange;
-      return currentTopic;
-    });
-
-    this.getTopics();
-  }
-
   editTopic(oldName) {
     console.log("Edit topic " + name);
     let alert = this.alertCtrl.create({
@@ -148,17 +131,13 @@ export class TopicsPPage {
                   child.once('value', (snapshot) => {
                     var votes = snapshot.val().voteCount;
                     this.removeTopics(oldName);
-                    // Update map, if was checked remove vote in the next line
-                    var oldStatus = this.topicsCheckedMap.get(oldName);
-                    this.topicsCheckedMap.delete(oldName);
-                    this.topicsCheckedMap.set(data.newName, oldStatus); // Add new name to map with its old status
                     this.addSpecificTopic(data.newName, votes);
                     this.getTopics();
                   });
                 }
               });
             }
-            else // Nothing entered ; do nothing
+            else // Nothing entered; do nothing
             {
               let noChangeAlert = this.alertCtrl.create({
                 title: 'No topic name entered!',
@@ -177,20 +156,7 @@ export class TopicsPPage {
   removeTopics(topicName) {
     this.topicsReady = false;
     this.firebaseProvider.removeTopics(this.className, this.lectureName, topicName);
-    this.topicsCheckedMap.delete(topicName);
     this.getTopics();
-  }
-
-  setStatus(topicName) {
-    var currentStatus = this.topicsCheckedMap.get(name);
-    if (currentStatus) // If not first time, just flip the status
-    {
-      this.topicsCheckedMap.set(name, !currentStatus);
-    }
-    else // First time
-    {
-      this.topicsCheckedMap.set(name, true);
-    }
   }
 
   presentDuplicateAlert() {
